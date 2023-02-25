77ac10e9724e6370c574193aa58949031e53873b
Leighton Hughes <leightonhughes@users.noreply.github.com>
2023-02-25 14:59:40 +1100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,4 +2,32 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   base: '/'
+  
+  lang: 'en-US',
+  title: 'Parish Handbook',
+  description: '',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Changelog', link: '/changelog' },
+    ],
+
+    sidebar: [
+      {
+          text: 'Control Panel',
+          items: [
+              { text: 'Example Link One', link: '/entries' },
+              { text: 'Example Link Two', link: '/globals' },
+          ],
+      },
+      {
+          text: 'Addons',
+          items: [
+              { text: 'Campaign', link: '/campaign' },
+              { text: 'Newsletter Builder', link: '/newsletter-builder' },
+          ],
+      },
+    ],
+  },
 })

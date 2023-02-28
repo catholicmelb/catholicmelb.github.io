e957939b29bf8ba00ecaa365660b7be75fa96e44
Leighton Hughes <hello@leightonhughes.co.nz>
2023-02-28 14:16:17 +1100

    Update config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -22,33 +22,33 @@ export default defineConfig({
       { 
           text: 'Contents',
           items: [
-              { text: '1. Overview', link: '1-overview.md' },
-              { text: '2. Principles of Parish Administration', link: '2-principles-of-parish-administration.md' },
+              { text: '1. Overview', link: '/1-overview.md' },
+              { text: '2. Principles of Parish Administration', link: '/2-principles-of-parish-administration.md' },
           ],
       },
 
-      { 
-          text: 'Guide',
-          items: [
-              { text: 'Example 1', link: 'guide/example-01.md' },
-              { text: 'Example 2', link: 'guide/example-02.md' },
-              { text: 'Example 3', link: 'guide/example-03.md' },
-          ],
-      },
+      // { 
+      //     text: 'Guide',
+      //     items: [
+      //         { text: 'Example 1', link: '/guide/example-01.md' },
+      //         { text: 'Example 2', link: '/guide/example-02.md' },
+      //         { text: 'Example 3', link: '/guide/example-03.md' },
+      //     ],
+      // },
 
       { 
           text: '1. Overview',
           items: [
-              { text: '1.1 Aim', link: 'aim' },
-              { text: '1.2 Church & Civil Law', link: 'law' },
+              { text: '1.1 Aim', link: '/aim' },
+              { text: '1.2 Church & Civil Law', link: '/law' },
           ],
       },
 
       { 
           text: '2. Principles of Parish Administration',
           items: [
-              { text: '2.1 General Principles', link: 'general-principles' },
-              { text: '2.2. Acts of Parish Administration', link: 'acts-of-parish-administration' },
+              { text: '2.1 General Principles', link: '/general-principles' },
+              { text: '2.2. Acts of Parish Administration', link: '/acts-of-parish-administration' },
           ],
       },
     ],
